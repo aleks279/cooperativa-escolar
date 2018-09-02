e33a55e8e7c4fccaabe7f0ed1712f430be1ab7a8
Saul Zamora <szamora@pernix-solutions.com>
2018-09-02 00:16:39 -0600

    Visual fixes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,15 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :find_user, only: %[show]
+
   def index
+  end
+
+  def show
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
