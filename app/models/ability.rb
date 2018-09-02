bab971ddffa53ae0372cd22d04ef718773261212
Saul Zamora <szamora@pernix-solutions.com>
2018-09-02 15:57:18 -0600

    Add some more role restrictions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   def initialize(user)
     if user.admin?
       can :manage, :all
+    elsif user.vendedor?
+      can :update, User, id: user.id
+    elsif user.personal_escolar?
+      can :update, User, id: user.id
     end
   end
 end
