b82bebe987b223d516a4f13cd32eb253d6e044cb
Saul Zamora <szamora@pernix-solutions.com>
2018-09-05 14:04:44 -0600

    varas de orders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require cocoon
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_012141) do
+ActiveRecord::Schema.define(version: 2018_09_04_200517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_012141) do
     t.boolean "available", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
