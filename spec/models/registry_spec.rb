ea0ff66058e8364388b9ef72e1aba402700d6154
Saul Zamora <szamora@pernix-solutions.com>
2018-10-01 19:37:07 -0600

    Add tests

--- spec/models/registry_spec.rb
+++ spec/models/registry_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Registry do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:time) }
     it { is_expected.to validate_presence_of(:registry_type) }
   end
 
