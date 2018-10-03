3c55e9c22b4cbda4d74d5e0dd264387e653fe931
Jeffrey <jeffny2015@gmail.com>
2018-10-03 17:21:22 -0600

    consultas de los productos mas vendidos

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,16 @@ class ReportsController < ApplicationController
   before_action :set_products, only: %i[new create edit update]
 
   def index
-    @orders = Order.all
+    #@orders_per_month = OrderItem.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year)
+
+    @cantidad_productos = OrderItem.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year).group(:product_id).count
+    @cantidad_productos_aux = @cantidad_productos.sort_by{|k,v| v}.reverse
+
+    @masvendidos = Hash.new
+
+    @cantidad_productos_aux.each do |k,v|
+      @masvendidos[Product.find(k).name] = v
+    end  
 
     respond_to do |format|
       format.html
@@ -57,3 +66,19 @@ class ReportsController < ApplicationController
     )
   end
 end
+
+
+#Order.where("MONTH(date_column) = ? and DAY(date_column) = ?", DateTime.now.month, DateTime.now.day)
+#Order.select("EXTRACT( month from created_at::date) as date")  
+#Consulta por mes
+#Order.where("EXTRACT( month from created_at::date)::integer = '?' ",DateTime.now.month)
+
+#Order.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year)
+
+
+
+
+#Best Sellers
+
+#OrderItem.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year).group(:product_id).count
+#a.sort_by{|k,v| v}.reverse
