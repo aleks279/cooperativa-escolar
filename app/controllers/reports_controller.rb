ce2c41d1c23b517c454419a652c43b03bf444307
Jeffrey <jeffny2015@gmail.com>
2018-10-04 18:14:01 -0600

    pull request

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,4 @@
 class ReportsController < ApplicationController
-  before_action :find_order, except: %i[index new create report]
-  before_action :set_products, only: %i[new create edit update]
 
   def index
     #@orders_per_month = OrderItem.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year)
@@ -56,42 +54,4 @@ class ReportsController < ApplicationController
 
   def report
   end
-
-  private
-
-  def find_order
-    @order = Order.find(params[:id])
-  end
-
-  def set_products
-    @products = Product.where(available: true)
-  end
-
-  def order_params
-    params.require(:order).permit(
-      :number, :date, :notes, :seller, :customer,
-      order_items_attributes: [
-        :id, :amount, :order, :product_id, :_destroy
-      ],
-      customer_attributes: [
-        :first_name, :last_name, :email,
-      ]
-    )
-  end
 end
-
-
-#Order.where("MONTH(date_column) = ? and DAY(date_column) = ?", DateTime.now.month, DateTime.now.day)
-#Order.select("EXTRACT( month from created_at::date) as date")  
-#Consulta por mes
-#Order.where("EXTRACT( month from created_at::date)::integer = '?' ",DateTime.now.month)
-
-#Order.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year)
-
-
-
-
-#Best Sellers
-
-#OrderItem.where("EXTRACT( month from created_at::date)::integer = '?' and EXTRACT( year from created_at::date)::integer = '?'", DateTime.now.month,DateTime.now.year).group(:product_id).count
-#a.sort_by{|k,v| v}.reverse
